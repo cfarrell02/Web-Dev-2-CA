ab0cc718a5ba596a216ef1c11d4ffa601a79679a
Glitch (hello-webpage) <none>
2021-02-04 15:53:21 +0000

    ✂️🐒 Checkpoint ./script.js:27940396/875 ./dashboard.html:27940396/666

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ function hideWelcome() {
   let welcomeUserDiv = document.getElementById("welcomeuser");
   welcomeUserDiv.style.display = "none";
 }
+const sonatas = {
+  title: 'Beethoven Sonatas',
+  songs: [
+    {
+      title: 'Piano Sonata No. 3',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 7',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 10',
+      artist: 'Beethoven',
+    }
+  ]
+};
+$(document).ready(function(){
+
+  $("#tableoutput").html("<h2 class='ui header'>" + sonatas.title + "</h2>");
+
+  $("#tableoutput").append("<table class='ui fixed striped table'><thead><tr><th>Song</th><th>Artist</th></tr></thead><tbody>");
+
+  for (let i in sonatas.songs){
+        $("#tableoutput tr:last").after("<tr><td>" + sonatas.songs[i].title + "</td><td>" + sonatas.songs[i].artist + "</td></tr>");
+  }
+
+  $("#tableoutput").append("</tbody></table>");
+  
+  
+
+});
