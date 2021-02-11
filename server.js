4b6f59c097edf8b9c71b04b653c84b1b591b5973
Glitch (hello-webpage) <none>
2021-02-11 15:41:01 +0000

    🍛🐑 Checkpoint ./package.json:27940396/36 ./server.js:27940396/189 ./views/layouts/main.hbs:27940396/1081

--- server.js
+++ server.js
@@ -4,12 +4,20 @@
 // import all required modules
 const logger = require('./utils/logger');
 const express = require("express");
+const exphbs = require('express-handlebars');
 
 // initialise project
 const app = express();
 
 // static files output to public folder
 app.use(express.static("public"));
+
+// use handlebars as view engine
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+}));
+app.set('view engine', '.hbs');
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
