9270642b0d2972f64f206de3b9085ac34e44205a
Glitch (hello-webpage) <none>
2021-02-11 16:37:06 +0000

    🚴🌹 Checkpoint ./routes.js:27940396/204 ./views/partials/menu.hbs:27940396/114

--- routes.js
+++ routes.js
@@ -4,13 +4,16 @@
 const express = require('express');
 const router = express.Router();
 
+
 // import controllers
 const start = require('./controllers/start.js');
 const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
 
 // connect routes to controllers
 router.get('/', start.index);
 router.get('/dashboard', dashboard.index);
+router.get('/about', about.index)
 
 // export router module
 module.exports = router;
