ab7804b2758c007275ee562f2fc62015fc66ddbc
Glitch (hello-webpage) <none>
2021-03-04 16:30:20 +0000

    😬🍟 Checkpoint ./controllers/dashboard.js:27940396/1378 ./controllers/playlist.js:27940396/2236 ./views/partials/listsongs.hbs:27940396/139

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,31 +1,18 @@
 'use strict';
 
-const _ = require('lodash');
 const logger = require('../utils/logger');
+const playlistStore = require('../models/playlist-store');
 
-const playlistStore = {
-
-  playlistCollection: require('./playlist-store.json').playlistCollection,
-
-  getAllPlaylists() {
-    return this.playlistCollection;
-    
-  },
-
-    getPlaylist(id) {
-    return _.find(this.playlistCollection, { id: id });
-  },
-    deleteSong(request, response) {
-      
+const playlist = {
+  index(request, response) {
     const playlistId = request.params.id;
-    const songId = request.params.songid;
-    logger.debug(`Deleting Song ${songId} from Playlist ${playlistId}`);
-    playlistStore.removeSong(playlistId, songId);
-    response.redirect('/playlist/' + playlistId);
-    
+    logger.debug('Playlist id = ' + playlistId);
+    const viewData = {
+      title: 'Playlist',
+      playlist: playlistStore.getPlaylist(playlistId),
+    };
+    response.render('playlist', viewData);
   },
-
 };
 
-module.exports = playlistStore;
-
+module.exports = playlist;
