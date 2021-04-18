b78d674a7c14b7d360b79abc41ae30903577dc8e
Glitch (hello-webpage) <none>
2021-04-18 19:39:48 +0000

    👴🚘 Checkpoint ./public/style.css:27940396/166 ./controllers/dashboard.js:27940396/6574

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,85 +1,90 @@
-'use strict';
+"use strict";
 
 // import all required modules
-const logger = require('../utils/logger');
-const leagueStore = require('../models/league-store.js');
-const uuid = require('uuid');
-const accounts = require ('./accounts.js');
+const logger = require("../utils/logger");
+const leagueStore = require("../models/league-store.js");
+const uuid = require("uuid");
+const accounts = require("./accounts.js");
 
 // create dashboard object
 const dashboard = {
-  
-  
   // index method - responsible for creating and rendering the view
-    index(request, response) {
-      
-logger.info('dashboard rendering');
+  index(request, response) {
+    logger.info("dashboard rendering");
     const loggedInUser = accounts.getCurrentUser(request);
     if (loggedInUser) {
-      
-             // app statistics calculations
-      const userLeagues = leagueStore.getUserLeagues(loggedInUser.id)
-  let numLeagues = userLeagues.length
-  let numTeams = 0;
+      // app statistics calculations
+      const userLeagues = leagueStore.getUserLeagues(loggedInUser.id);
+      let numLeagues = userLeagues.length;
+      let numTeams = 0;
 
-    for (let item of userLeagues) {
-    numTeams += item.teams.length;
-  }
+      for (let item of userLeagues) {
+        numTeams += item.teams.length;
+      }
+      let averageTeams = 0;
+      if (!(userLeagues.length === 0)) {
+        averageTeams = (numTeams / numLeagues).toFixed(1); // https://www.w3schools.com/jsref/jsref_tofixed.asp
+      }
+      let leagueMostItemsName = "No Leagues";
+      if (!(userLeagues.length === 0)) {
+        let leagueMostItems = userLeagues[0];
+        for (let league of userLeagues) {
+          if (league.teams.length > leagueMostItems.teams.length) {
+            leagueMostItems = league;
+          }
+        }
+        leagueMostItemsName = leagueMostItems.name;
+      }
 
-  let averageTeams = (numTeams/numLeagues).toFixed(1); // https://www.w3schools.com/jsref/jsref_tofixed.asp
+      let leagueLeastItemsName = "No Leagues";
+      if (!(userLeagues.length === 0)) {
+        let leagueLeastItems = userLeagues[0];
+        for (let league of userLeagues) {
+          if (league.teams.length < leagueLeastItems.teams.length) {
+            leagueLeastItems = league;
+          }
+        }
+        leagueLeastItemsName = leagueLeastItems.name;
+      }
 
-       let leagueMostItems = userLeagues[0];
- for(let league of userLeagues){
-   if(league.teams.length>leagueMostItems.teams.length){
-     leagueMostItems = league;
-   }
- }
-    let leagueLeastItems = userLeagues[0];
- for(let league of userLeagues){
-   if(league.teams.length<leagueLeastItems.teams.length){
-     leagueLeastItems = league;
-   }
- }
-  
-    const viewData = {
-      title: 'League Dashboard',
-      leagues: leagueStore.getUserLeagues(loggedInUser.id),
-      totalLeagues: numLeagues,
-      totalTeams:numTeams,
-      averageTeams:averageTeams,
-      leagueMostItems: leagueMostItems.name,
-      leagueLeastItems: leagueLeastItems.name,
-      fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
-      picture: loggedInUser.picture
-    };
-    logger.info('about to render' + viewData.leagues);
-    response.render('dashboard', viewData);
-    }
-    else response.redirect('/');
+      const viewData = {
+        title: "League Dashboard",
+        leagues: leagueStore.getUserLeagues(loggedInUser.id),
+        totalLeagues: numLeagues,
+        totalTeams: numTeams,
+        averageTeams: averageTeams,
+        leagueMostItems: leagueMostItemsName,
+        leagueLeastItems: leagueLeastItemsName,
+        fullname: loggedInUser.firstName + " " + loggedInUser.lastName,
+        picture: loggedInUser.picture
+      };
+      logger.info("about to render" + viewData.leagues);
+      response.render("dashboard", viewData);
+    } else response.redirect("/");
   },
-  
+
   deleteLeague(request, response) {
     const leagueId = request.params.id;
     logger.debug(`Deleting league ${leagueId}`);
     leagueStore.removeLeague(leagueId);
-    response.redirect('/dashboard');
+    response.redirect("/dashboard");
   },
-  
-    addLeague(request, response) {
+
+  addLeague(request, response) {
     const loggedInUser = accounts.getCurrentUser(request);
     const newLeague = {
       id: uuid(),
       userid: loggedInUser.id,
       name: request.body.name,
-      country:request.body.country,
-      continent:request.body.continent,
-      logo:request.files.logo,
-      teams: [],
+      country: request.body.country,
+      continent: request.body.continent,
+      logo: request.files.logo,
+      teams: []
     };
-    leagueStore.addLeague(newLeague , function(){
-    response.redirect('/dashboard');
-    })
-  },
+    leagueStore.addLeague(newLeague, function() {
+      response.redirect("/dashboard");
+    });
+  }
 };
 
 // export the dashboard module
