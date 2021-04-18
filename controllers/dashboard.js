f50e39f81ed83e205abf2922198b3b8d19ce7936
Glitch (hello-webpage) <none>
2021-04-18 15:19:39 +0000

    🍣🐃 Checkpoint ./controllers/dashboard.js:27940396/821 ./views/dashboard.hbs:27940396/2260

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -9,17 +9,28 @@ const accounts = require ('./accounts.js');
 // create dashboard object
 const dashboard = {
   
-  // app statistics calculations
-  
   
   // index method - responsible for creating and rendering the view
     index(request, response) {
-    logger.info('dashboard rendering');
+      
+logger.info('dashboard rendering');
     const loggedInUser = accounts.getCurrentUser(request);
     if (loggedInUser) {
+      
+             // app statistics calculations
+  let numLeagues = leagueStore.getUserLeagues(loggedInUser.id).length
+  let numTeams = 0;
+
+    for (let item of leagueStore.getUserLeagues(loggedInUser.id)) {
+    numTeams += item.teams.length;
+  }
+  
+  
     const viewData = {
       title: 'League Dashboard',
       leagues: leagueStore.getUserLeagues(loggedInUser.id),
+      totalLeagues: numLeagues,
+      totalTeams:numTeams,
       fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
     };
     logger.info('about to render' + viewData.leagues);
