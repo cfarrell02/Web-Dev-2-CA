747604a5e901a9ba785c4a3496f98e254d662b5a
Glitch (hello-webpage) <none>
2021-04-19 10:48:03 +0000

    ⛸📹 Checkpoint ./public/style.css:27940396/214 ./views/partials/listcomments.hbs:27940396/76 ./server.js:27940396/78

--- server.js
+++ server.js
@@ -35,3 +35,7 @@ app.use('/', routes);
 const listener = app.listen(process.env.PORT || 4000, function () {
   logger.info('Your app is listening on port ' + listener.address().port);
 });
+
+formatDate: function(date){
+  
+}
