c33a9b476d120560f928976985186eec962c2c4a
Glitch (hello-webpage) <none>
2021-04-19 10:59:23 +0000

    🍘🍀 Checkpoint ./server.js:27940396/2946

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // use javascript in strict mode
-'use strict';
+"use strict";
 
 // import all required modules
 const express = require("express");
-const logger = require('./utils/logger');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
+const logger = require("./utils/logger");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 // initialise project
 const app = express();
@@ -16,26 +16,41 @@ const app = express();
 app.use(express.static("public"));
 
 // use bodyParser, cookieParser, fileUpload
-app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
 
 // use handlebars as view engine
-app.engine('.hbs', exphbs({
-  extname: '.hbs',
-  defaultLayout: 'main',
-}));
-app.set('view engine', '.hbs');
+app.engine(
+  ".hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "main",
+    helpers: {
+      //helpers go here
+      formatDate: function(date) {
+        let dateCreated = new Date(date);
+        let day = dateCreated.getDay();
+        let dateNum = dateCreated.getDate();
+        let month = dateCreated.getMonth();
+        let year = dateCreated.getFullYear();
+        
+        let days = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"]
+        let months = ["Jan","Feb","March","April","May","June","July","Aug","Sept","Oct","Nov","Dec"]
+        let monthName = months[month];
+        let 
+        
+      }
+    }
+  })
+);
+app.set("view engine", ".hbs");
 
 // import routes file and use this for routing
-const routes = require('./routes');
-app.use('/', routes);
+const routes = require("./routes");
+app.use("/", routes);
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT || 4000, function () {
-  logger.info('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 4000, function() {
+  logger.info("Your app is listening on port " + listener.address().port);
 });
-
-formatDate: function(date){
-  
-}
