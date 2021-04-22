7c8a7a56095fcc827139a68ffc69710a9f0443b4
Glitch (hello-webpage) <none>
2021-04-22 15:37:29 +0000

    ⛵️🌉 Checkpoint ./server.js:27940396/91

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.engine(
       },
       // found at: https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value 
       ifEquals: function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    return ((arg1 == arg2)||(arg2==1)) ? options.fn(this) : options.inverse(this); //Second option added to give account with id 1, access to all delete/edits
   }
     }
   }))
